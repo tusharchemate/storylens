dee623190660d9050ea7ed1477e66f5bc29b896b
“Tushar” <abc@example.com>
2023-12-23 19:33:56 +0530

    added post card with stats

---
+++ src/components/shared/SearchResults.tsx
@@ -0,0 +1,24 @@
+import { Models } from "appwrite";
+import Loader from "./Loader";
+import GridPostList from "./GridPostList";
+
+interface SearchResultsProps {
+  searchPosts?: Models.Document;
+  isSearchFetching: boolean;
+}
+
+const SearchResults = ({
+  searchPosts,
+  isSearchFetching,
+}: SearchResultsProps) => {
+  if (isSearchFetching) {
+    return <Loader />;
+  }
+
+  if (searchPosts?.documents?.length > 0) {
+    return <GridPostList posts={searchPosts?.documents} />;
+  }
+  return <p className="text-light-4 mt-10 text-center w-full">No Results</p>;
+};
+
+export default SearchResults;
---
+++ src/components/shared/Topbar.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "../ui/button";
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutation";
+import { useUserContext } from "@/context/AuthContext";
+
+const Topbar = () => {
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+
+  const navigate = useNavigate();
+
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate("/sign-in");
+    }
+  }, [isSuccess]);
+
+  return (
+    <section className="topbar">
+      <div className="flex-between py-4 px-5">
+        <Link to="/" className="flex gap-3 items-center">
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            width={35}
+            height={35}
+          />
+        </Link>
+        <div className="flex gap-4">
+          <Button variant="ghost" onClick={() => signOut()}>
+            <img src="/assets/icons/logout.svg" alt="logout" />
+          </Button>
+
+          <Link to={""}>
+            {/*  to={`/profile/${user.id}`} */}
+            <img src={user.imageUrl} className="h-8 w-8 rounded" />
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Topbar;
