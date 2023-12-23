fb8793da6b2f42e7122cdc72c31bc2f6ba3635d4
“Tushar” <abc@example.com>
2023-12-23 19:32:02 +0530

    added root layout

---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,21 @@
+import Footer from "@/components/shared/Footer";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Topbar from "@/components/shared/Topbar";
+import { Outlet } from "react-router-dom";
+
+const RootLayout = () => {
+  return (
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
+
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default RootLayout;
