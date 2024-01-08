bf30586caf49e8ac17bc0b2b1c5a3cc37e7c5aaa
Tushar <tusharchemate07@gmail.com>
2024-01-08 22:51:56 +0530

    code refactored

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -8,11 +8,9 @@ const RootLayout = () => {
     <div className="w-full md:flex">
       <Topbar />
       <LeftSidebar />
-
       <section className="flex flex-1 h-full">
         <Outlet />
       </section>
-
       <Footer />
     </div>
   );
